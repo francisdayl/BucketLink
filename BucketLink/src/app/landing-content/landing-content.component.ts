31077aff9107473f7d2d78a89389229ab71cf937
stefany <stelsant@espol.edu.ec>
2022-02-01 04:59:30 -0500

    landing page casi lista

--- BucketLink/src/app/landing-content/landing-content.component.ts
+++ BucketLink/src/app/landing-content/landing-content.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-content.component.css']
 })
 export class LandingContentComponent implements OnInit {
-
+  focus: any;
+  focus1: any;
   constructor() { }
 
   ngOnInit(): void {
